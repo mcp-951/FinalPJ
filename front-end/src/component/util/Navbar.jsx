74345cd02458e31c7bb26a34cffd594bbc30e8c4
m747op <m747op@gmail.com>
2024-10-14 09:47:51 +0900

    SeachBar추가중

--- front-end/src/component/util/Navbar.jsx
+++ front-end/src/component/util/Navbar.jsx
@@ -25,34 +25,43 @@ function Navbar(){
     const handleDropdown = (menu) => {
         setMenuOpen(menuOpen === menu ? null : menu);
     };
+    const [searchOpen, setSearchOpen] = useState(false);
+
+    const handleSearchBar = () => {
+        setSearchOpen(!searchOpen);
+    };
     return(
         <>
-        <div className="navbar_header">
-            <div className="nav_logo"><a href='/'><img src="/images/main/logo2.png" /></a></div>
-            <div className='nav_menu'>
-                <ul>
-                    <li onClick={() => handleDropdown('account')}><div>계좌</div></li>
-                    <li onClick={()=> handleDropdown('product')}><div>금융상품</div></li>
-                    <li onClick={()=> handleDropdown('exchange')}><div>외환</div></li>
-                    <li onClick={()=> handleDropdown('tax')}><div>공과금</div></li>
-                    <li onClick={()=> handleDropdown('manage')}><div>자산관리</div></li>
-                    <li onClick={()=> handleDropdown('investment')}><div>투자</div></li>
-                </ul>
+        { !searchOpen ? (
+            <div className="navbar_header">
+                <div className="nav_logo"><a href='/'><img src="/images/main/logo2.png" /></a></div>
+                <div className='nav_menu'>
+                    <ul>
+                        <li onClick={() => handleDropdown('account')}><div>계좌</div></li>
+                        <li onClick={()=> handleDropdown('product')}><div>금융상품</div></li>
+                        <li onClick={()=> handleDropdown('exchange')}><div>외환</div></li>
+                        <li onClick={()=> handleDropdown('tax')}><div>공과금</div></li>
+                        <li onClick={()=> handleDropdown('manage')}><div>자산관리</div></li>
+                        <li onClick={()=> handleDropdown('investment')}><div>투자</div></li>
+                    </ul>
+                </div>
+                <div className='nav_iconMenu'>
+                    <button onClick={handleSearchBar}><div className="nav_search"><FaSearch className='nav_search_icon'/></div></button>
+                    <button><div className="nav_allMenu"><GiHamburgerMenu className='nav_toggle_icon'/></div></button>
+                    {token ? (<button onClick={handleLogout}><div className="nav_logout"><CiLogout className="nav_logout_icon"/></div></button>)
+                    : (<button><div className="nav_login" ><a href='/login'><MdOutlineLogin className='nav_login_icon' /></a></div></button>)}
+                    
+                </div>
             </div>
-            <div className='nav_iconMenu'>
-                <button><div className="nav_search"><FaSearch className='nav_search_icon'/></div></button>
-                <button><div className="nav_allMenu"><GiHamburgerMenu className='nav_toggle_icon'/></div></button>
-                {token ? (<button onClick={handleLogout}><div className="nav_logout"><CiLogout className="nav_logout_icon"/></div></button>)
-                 : (<button><div className="nav_login" ><a href='/login'><MdOutlineLogin className='nav_login_icon' /></a></div></button>)}
-                
-            </div>
-        </div>
-        {menuOpen === 'account' && (<AccountDrop/>)}
-        {menuOpen === 'product' && (<ProductDrop/>)}
-        {menuOpen === 'exchange' &&(<ExchangeDrop/>)}
-        {menuOpen === 'tax' &&(<TaxDrop/>)}
-        {menuOpen === 'manage' &&(<ManageDrop/>)}
-        {menuOpen === 'investment' &&(<InvestmentDrop/>)}
+
+        ) : (<></>)
+        }
+            {menuOpen === 'account' && (<AccountDrop/>)}
+            {menuOpen === 'product' && (<ProductDrop/>)}
+            {menuOpen === 'exchange' &&(<ExchangeDrop/>)}
+            {menuOpen === 'tax' &&(<TaxDrop/>)}
+            {menuOpen === 'manage' &&(<ManageDrop/>)}
+            {menuOpen === 'investment' &&(<InvestmentDrop/>)}
         </>
     );
 
---
+++ front-end/src/component/util/searchBar/SearchBar.jsx
@@ -0,0 +1,11 @@
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+
+function SearchBar (){
+  return (
+    <>
+    </>
+  );
+}
+
+export default SearchBar;
