e978fd8ac7a3d0d7ea07fc5fceccb6b135f4b1be
DESKTOP-MD6RE2A\lenovo <dlehdaud19@gmail.com>
2024-10-18 15:03:09 +0900

    no message

--- src/main/java/com/urambank/uram/repository/SupportRepository.java
+++ src/main/java/com/urambank/uram/repository/SupportRepository.java
@@ -8,14 +8,14 @@ import java.util.List;
 
 @Repository
 public interface SupportRepository extends JpaRepository<SupportEntity, Integer> {
-<<<<<<< HEAD
+
     List<SupportEntity> findAllByUserId(Integer userId);
-=======
+
 
     // 특정 사용자의 삭제되지 않은 문의글 조회
     List<SupportEntity> findAllByUserIdAndIsDeleted(Integer userId, String isDeleted);
 
     // 삭제되지 않은 모든 문의글 조회
     List<SupportEntity> findAllByIsDeleted(String isDeleted);
->>>>>>> origin/mjs_mca
+
 }
